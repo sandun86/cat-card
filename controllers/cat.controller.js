b9c76cc218ac137b234988384cbbe5d5079b0a86
Sandun Dissanayake <java2012@gmail.com>
2023-07-12 19:54:58 +0530

    Update cat.controller.js

--- controllers/cat.controller.js
+++ controllers/cat.controller.js
@@ -2,7 +2,8 @@ const catService = require("../services/cat.service");
 
 const catController = {
 	blendImage: async (req, res) => {
-		return await catService.blendImages(req, res);
+		const blendImage = await catService.blendImages(req, res);
+		return blendImage;
 	},
 };
 
